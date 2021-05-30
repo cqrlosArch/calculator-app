795b02af6b4a043b435a7826fbf6f4a8d0c2490c
cqrlosArch <diaz.flores.c@gmail.com>
2021-05-30 13:22:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+
+<h1 align="center">Gulp Workflow </h1>
+
+# Gulp Workflow to compile javascript, css, html and images
+
+Workflow of pug, sass and esBuild for static page projects.
+
+Use Gulp version 4 to execute the tasks.
+
+Transpile the HTML, CSS and JavaScript so that it is possible to work in a development folder and another folder with all the minified and built files for uploading to the server
+
+Optimizes JPG, JPEG, GIF, PNG and SVG images
+
+Use Browser Sync to mode development.
+
+***
+
+<p align="center">
+  <img src="screenshot.png">
+</p>
+
+## How To Use
+
+To clone and run this application, you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) or [yarnjs](https://yarnpkg.com/) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/cqrlosArch/workflow-gulp
+
+# Install dependencies
+$ 'yarn install' or 'npm install'
+
+# Run script mode development
+$ 'yarn start' or 'npm start'
+
+# Run script mode production
+$ 'yarn build' or 'npm run build'
+```
+
+## Details
+
+### Built With
+
+- [Gulp](https://gulpjs.com/)
+- [esBuild](https://esbuild.github.io/)
+- [Sass](https://sass-lang.com/)
+- [Pug](https://pugjs.org/api/getting-started.html)
+- [Browser-Sync](https://browsersync.io/)
+- [Imagemin](https://github.com/imagemin/imagemin)
+
+## License
+
+[MIT License](./LICENSE)
+
+## Contact
+
+- Discord [CqrlosArch#0296](https://discord.com/)
+- Twitch [@cqrlosarch](https://www.twitch.tv/cqrlosarch/about)
+- GitHub [@cqrlosArch](https://github.com/cqrlosArch)
+- Twitter [@cqrlosArch](https://twitter.com/cqrlosArch)
+- Youtube [Cqrlos Arch](https://www.youtube.com/channel/UCV8eaXiCFXUN5Rkpc42G3ZQ)
---
+++ gulpfile.babel.js
@@ -0,0 +1,169 @@
+import gulp from 'gulp';
+import sourcemaps from 'gulp-sourcemaps';
+import rename from 'gulp-rename';
+
+import { init as server, stream, reload } from 'browser-sync';
+
+const gulpEsbuild_build = require('gulp-esbuild');
+const { createGulpEsbuild } = require('gulp-esbuild');
+const gulpEsbuild_dev = createGulpEsbuild();
+
+import plumber from 'gulp-plumber';
+
+import cacheBust from 'gulp-cache-bust';
+
+import pug from 'gulp-pug';
+
+import sass from 'gulp-dart-sass';
+import cssnano from 'cssnano';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+
+import purgecss from 'gulp-purgecss';
+
+import imagemin from 'gulp-imagemin';
+
+import htmlreplace from 'gulp-html-replace';
+
+const mode = require('gulp-mode')();
+
+const pluginsPostcss = [autoprefixer, cssnano];
+
+gulp.task('esbuild', async () => {
+  return gulp
+    .src('./src/js/index.js')
+    .pipe(plumber())
+    .pipe(mode.development(sourcemaps.init()))
+    .pipe(
+      mode.production(
+        gulpEsbuild_build({
+          outfile: 'bundle.min.js',
+          bundle: true,
+          minify: true,
+          target: 'es2015',
+        })
+      )
+    )
+    .pipe(
+      mode.development(
+        gulpEsbuild_dev({
+          outfile: 'bundle.js',
+          sourcemap: true,
+          bundle: true,
+        })
+      )
+    )
+    .pipe(mode.development(sourcemaps.write('.')))
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('views', () => {
+  return gulp
+    .src('./src/views/pages/*.pug')
+    .pipe(plumber())
+    .pipe(
+      pug({
+        pretty: !mode.production(),
+      })
+    )
+    .pipe(
+      cacheBust({
+        type: 'timestamp',
+      })
+    )
+    .pipe(
+      mode.production(
+        htmlreplace({
+          js: {
+            src: 'js/bundle.min.js',
+            tpl: '<script src="%s" type="module"></script>',
+          },
+          css: {
+            src: [
+              ['css/styles.min.css', 'js/bundle.min.js', 'css/styles.min.css'],
+            ],
+            tpl:
+              '<link rel="preload" href="%s" as="style"/><link rel="modulepreload" href="%s"/><link rel="stylesheet" href="%s"/>',
+          },
+        })
+      )
+    )
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('sass', async () => {
+  return gulp
+    .src('./src/scss/styles.scss')
+    .pipe(plumber())
+    .pipe(mode.development(sourcemaps.init()))
+    .pipe(
+      sass({
+        outputStyle: 'compressed',
+      }).on('error', sass.logError)
+    )
+    .pipe(mode.production(postcss(pluginsPostcss)))
+    .pipe(mode.production(rename('styles.min.css')))
+    .pipe(mode.development(sourcemaps.write('.')))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(stream());
+});
+
+gulp.task('imgMin', () => {
+  return gulp
+    .src('./src/assets/images/**/*')
+    .pipe(plumber())
+    .pipe(
+      mode.production(
+        imagemin([
+          imagemin.gifsicle({ interlaced: true }),
+          imagemin.mozjpeg({ quality: 75, progressive: true }),
+          imagemin.optipng({ optimizationLevel: 5 }),
+          imagemin.svgo({
+            plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+          }),
+        ])
+      )
+    )
+    .pipe(gulp.dest('./public/assets/images'));
+});
+
+gulp.task('fonts', () => {
+  return gulp
+    .src('./src/assets/fonts/**/*')
+    .pipe(plumber())
+    .pipe(gulp.dest('./public/assets/fonts'));
+});
+
+gulp.task('clean-css', () => {
+  return gulp
+    .src('./public/css/*.css')
+    .pipe(plumber())
+    .pipe(
+      purgecss({
+        content: ['./public/*.html'],
+      })
+    )
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', () => {
+  server({
+    server: './public',
+  });
+  gulp.watch('./src/views/**/*.pug', gulp.series('views')).on('change', reload);
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('./src/js/**/*.js', gulp.series('esbuild')).on('change', reload);
+  gulp.watch(
+    './src/assets/images/**/*.(svg|jpg|ico|png|jpeg)',
+    gulp.series('imgMin')
+  );
+});
+
+gulp.task(
+  'build',
+  gulp.series(
+    gulp.parallel('views', 'sass', 'esbuild'),
+    'clean-css',
+    'imgMin',
+  )
+);
