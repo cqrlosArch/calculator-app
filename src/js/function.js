94556f62820c4d86986a2e1f022de96af9591569
cqrlosArch <diaz.flores.c@gmail.com>
2021-05-31 18:16:32 +0200

    mediaQuery

--- src/js/function.js
+++ src/js/function.js
@@ -1,12 +1,19 @@
 const styles = document.documentElement.style;
 const display = document.getElementById('display');
 
+const mediaQuery = window.matchMedia('(min-width: 500px)').matches;
+
 //Functions Calculator
 let logarithm = '';
 
 const calculator = (log = logarithm) => {
   if (log === '') return;
-  logarithm = eval(log).toString().slice(0, 13);
+  if (mediaQuery) {
+    logarithm = eval(log).toString().slice(0, 16);
+  } else {
+    logarithm = eval(log).toString().slice(0, 13);
+  }
+
   return (display.textContent = logarithm);
 };
 
@@ -16,7 +23,11 @@ const reset = () => {
 };
 
 const addKey = (key) => {
-  logarithm += key;
+  if (mediaQuery) {
+    if (logarithm.length < 16) logarithm += key;
+  } else {
+    if (logarithm.length < 13) logarithm += key;
+  }
   return (display.textContent = logarithm);
 };
 
